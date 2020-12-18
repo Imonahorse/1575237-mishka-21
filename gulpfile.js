b57dfe7571f66c16f504af73f50a24ba148f998a
Alexey <Awotjan@gmail.com>
2020-12-18 17:15:43 +0300

    Добавил hover и active на svg

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,14 @@ const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const csso = require("postcss-csso");
-const autoprefixer = require("autoprefixer");
 const rename = require("gulp-rename");
-const imagemin = require("qulp-imagemin");
+const autoprefixer = require("autoprefixer");
+const htmlmin = require("gulp-htmlmin");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 const sync = require("browser-sync").create();
 
 // Styles
@@ -22,19 +27,41 @@ const styles = () => {
     ]))
     .pipe(sourcemap.write("."))
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Html
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({collapseWhitespace: true }))
+    .pipe(gulp.dest("dist"));
+}
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+    .pipe(uglify())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("dist/js"))
+    .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+
 
 // Image
 
 const images = () => {
   return gulp.src("source/img/**/*.{jpg,png,svg}")
     .pipe(imagemin([
-      imagemin.mozjpeg({progressive:true}),
-      imagemin.optipng({optimizationLevel:3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("dist/img"))
@@ -42,12 +69,56 @@ const images = () => {
 
 exports.images = images;
 
+// WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("dist/img"))
+}
+
+exports.createWebp = createWebp;
+
+
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/icons/*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("dist/img/icons"))
+}
+
+exports.sprite = sprite;
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+      "source/fonts/*.{woff2,woff}",
+      "source/*.ico",
+      "source/img/**/*.{jpg,png,svg}"
+    ],
+    {
+      base: "source"
+    })
+    .pipe(gulp.dest("dist"))
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("dist");
+}
+
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'dist'
     },
     cors: true,
     notify: false,
@@ -62,9 +133,38 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html", gulp.series(html, sync.reload));
 }
 
+// Build
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    copy,
+    images,
+    createWebp
+  )
+)
+
+exports.build = build;
+
+
 exports.default = gulp.series(
-  styles, server, watcher
-);
+  clean,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    copy,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
