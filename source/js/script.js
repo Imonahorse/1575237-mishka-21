e696469d0ef26e760fb102f6e336c4919644bf96
Alexey <Awotjan@gmail.com>
2020-12-21 01:57:51 +0300

    index mobile full pp fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// КНОПКА БУРГЕР
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -11,4 +13,25 @@ navToggle.addEventListener('click', function () {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
+});
+
+// МОДАЛЬНОЕ ОКНО
+
+var orderButton = document.querySelector(".card__button");
+var modalButton = document.querySelector(".modal__button");
+var orderPopup = document.querySelector(".modal__order");
+var modalItem = document.querySelector(".modal__item--current");
+
+
+orderButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  orderPopup.classList.remove("modal__order--close");
+  orderPopup.classList.add("modal__order--open");
+  modalItem.focus();
+});
+
+modalButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  orderPopup.classList.remove("modal__order--open");
+  orderPopup.classList.add("modal__order--close");
 });
